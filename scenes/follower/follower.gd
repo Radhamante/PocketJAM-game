12f2dadcf471be4a3f4a2847aca6a1610d91bf9c
Radhamante <raphael.delassus@proton.me>
2025-07-05 14:11:31 +0200

    fix : swarm

--- scenes/follower/follower.gd
+++ scenes/follower/follower.gd
@@ -2,18 +2,20 @@ extends CharacterBody3D
 
 class_name Follower
 
-@export var speed := 5.0
+@export var speed := 8.0
 @export var response_speed := 6.0
 @export var neighbor_distance := 10.0
 @export var separation_distance := 3.0
 @export var min_player_distance := 2.0
-@export var hold_player_distance := 3.0
 
-@export var weight_cohesion := 0.5
-@export var weight_separation := 1.5
-@export var weight_alignment := 0.3
-@export var weight_target := 1.0
+@export var weight_cohesion := 1
+@export var weight_separation := 1
+@export var weight_alignment := 1
+@export var weight_target := 2.0
 
+
+var hold_player_distance: float
+var navigation: NavigationRegion3D
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var player: Node3D
 var swarm_root: Node
@@ -22,6 +24,7 @@ var vertical_velocity := 0.0
 
 func _ready() -> void:
 	swarm_root = get_parent_node_3d()
+	hold_player_distance = randf_range(2,7)
 
 func _physics_process(delta):
 	if not player or not swarm_root:
@@ -54,7 +57,14 @@ func _physics_process(delta):
 	var distance_to_player = global_position.distance_to(player.global_position)
 
 	if distance_to_player > hold_player_distance:
-		var to_player = (player.global_position - global_position).normalized()
+		var nav_map = navigation.get_navigation_map()
+		var path = NavigationServer3D.map_get_path(nav_map, global_position, player.global_position, false)
+
+		var target_point = global_position
+		if path.size() > 1:
+			target_point = path[1]
+
+		var to_player = (target_point - global_position).normalized()
 		direction = (
 			cohesion * weight_cohesion +
 			separation * weight_separation +
