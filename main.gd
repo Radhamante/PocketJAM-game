ebfac716d35c45089818640a86c7b959426af089
Radhamante <raphael.delassus@proton.me>
2025-07-06 12:16:17 +0200

    feat :things

--- main.gd
+++ main.gd
@@ -3,18 +3,36 @@ extends Node3D
 signal game_ended(day_time: float, gamer_saved: int)
 
 @export var sun: DirectionalLight3D
-@export var day_duration = 3.0
+@export var day_duration = 20
 
+@export_range(0.0,365.0) var sun_hue = 16.0
+@export_range(0.0,100.0) var sun_max_saturation = 37.0
 
 var gamer_saved = 0
 var day_time = 0.0
+
+func _ready() -> void:
+	sun_hue = sun_hue/365
+	sun_max_saturation = sun_max_saturation/100
 
 func _process(delta: float) -> void:
 	day_time += delta
 	sun.rotation_degrees.x = remap(day_time, 0, day_duration, 0, -270)
 	
+	var sun_color_saturation = 0
+	var third_of_day = day_duration / 3
+	
+	if day_time < third_of_day:
+		sun_color_saturation = remap(day_time, 0, third_of_day, sun_max_saturation, 0)
+	elif day_time < (third_of_day)*2:
+		sun_color_saturation = remap(day_time, third_of_day, third_of_day*2, 0, sun_max_saturation)
+	else:
+		sun_color_saturation = remap(day_time, third_of_day, third_of_day*2, 0, sun_max_saturation)
+			
+	sun.light_color = Color.from_hsv(sun_hue, sun_color_saturation, 1)
+	
 	if day_time >= day_duration:
-		day_time = 3.0
+		day_time = day_duration
 		_day_ended()
 	
 	
